c28d67c24c3ba62ff69416f43ed33adb43f9c5aa
shravan-mohan <70625467+shravan-mohan@users.noreply.github.com>
2025-11-22 08:12:22 +0000

    Deploying to gh-pages from @ shravan-mohan/shravan-mohan.github.io@df6a939408a984d459027732d6dcaeea75f11462 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -437,7 +437,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Shravan_Mohan_CV_updated.pdf", "_blank");
         },
       },{
         id: 'social-email',
